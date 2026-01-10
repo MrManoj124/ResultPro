f69cf3bf93eba164166d740a6da75b62ceb75b3f
MrManoj124 <manojmanorooban@gmail.com>
2026-01-10 13:43:06 +0530

    modify frontpage.js file

--- frontend/src/components/frontpage.js
+++ frontend/src/components/frontpage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../CSS/Frontpage.css";
+import "../CSS/FrontPage.css";
 
 function FrontPage() {
   const navigate = useNavigate();
